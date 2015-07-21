ecdae249bdee10df1ad43490897938b3bfeb6f45
Milo Gutiérrez <MiloGutierrez91@Gmail.com>
2015-07-21 00:54:31 -0300

    Agregada clase ChatRoomData.cs

---
+++ ChatRoomData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FabSample
+{
+	public class ChatRoomData
+	{
+		public Room Room { get; set; }
+		public List<Message> Messages { get; set; }
+		public Boolean Available { get; set; }
+	}
+}
+
